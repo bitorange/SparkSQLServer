b0ebcf6e18f7e7289644115e0dab6a3bc4dbd2b6
bitorange <bitorange@foxmail.com>
2015-03-28 20:27:24 +0800

    Add: 增加连接队列服务器的类

--- src/main/java/service/JerseyServer.java
+++ src/main/java/service/JerseyServer.java
@@ -3,57 +3,65 @@ package service;
 /**
  * Created by orange on 2015/3/17.
  */
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
-import cn.edu.bit.linc.*;
-import org.json.JSONArray;
+import cn.edu.bit.linc.AccountCheck;
+import cn.edu.bit.linc.ConnectJDBC;
+import cn.edu.bit.linc.JDBCUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import cn.edu.bit.linc.FieldConverter.*;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Path("/service")
 public class JerseyServer {
+    private HQLFieldsConverter fieldsConverter = new HQLFieldsConverter();    // A converter used to convert the result of SQL command
 
     /**
-     * 接受传入的SQL语句，然后执行SQL语句，return结果
+     * 接受传入的SQL语句，然后执行 SQL 语句，return结果
      * @param     sqlstring  传入的SQL语句
      * @return    返回SQL语句查询的结果
      */
     @GET
     @Path("/{sql}")
     public Response helloWorld(@PathParam("sql") String sqlstring) {
+        String response = null;
+        String newSqlString = null;
 
-        String response=null;
-        String  newSqlString = null;
         try {
-            newSqlString = java.net.URLDecoder.decode(sqlstring,   "utf-8");
+            newSqlString = java.net.URLDecoder.decode(sqlstring, "utf-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
         System.out.println(newSqlString);
+
+        // 连接数据库，执行命令
         ConnectJDBC conn = new ConnectJDBC();
-        ResultSet rs=null;
+        ResultSet rs;
+        ArrayList<HashMap<String, String>> result;
         try {
             rs = conn.getAndExucuteSQL(newSqlString);
+            result = fieldsConverter.parseCommand(newSqlString, rs);
         }catch (Exception e){
-
             return Response.status(200).entity(e.getMessage().toString()).build();
         }
 
+        // 将 ResultSet 转换为 JSON Object
         JSONObject jsonObject;
         try {
-            jsonObject = conn.transformToJsonArray(rs);
-            response=jsonObject.toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }finally{
+            // jsonObject = conn.transformToJsonArray(rs);
+            jsonObject = conn.convertArrayListToJsonOjbect(result);
+            response = jsonObject.toString();
+        } finally{
             JDBCUtils.releaseAll();
         }
 
-        //将结果转换成json数据结构返回
+        // 将结果转换成 JSON 返回
         return Response.status(200).entity(response).build();
     }
 
@@ -70,11 +78,10 @@ public class JerseyServer {
     public String check(@QueryParam("name") String name,
                         @DefaultValue("26") @QueryParam("password") String password) {
         System.out.println(name);
-        //获取name和password，进行校验
-
+        // 获取name和password，进行校验
         String result=AccountCheck.checkAccount(name,password);
 
-        //返回结果
+        // 返回结果
         if(result.equals("登入成功"))
             return "ok";
         else
---
+++ src/main/java/service/RPCConnection.java
@@ -0,0 +1,165 @@
+package service; /**
+ * Created by orange on 2015/3/27.
+ */
+
+import com.rabbitmq.client.*;
+//import dnl.utils.text.table.TextTable;
+//import net.sf.json.JSONArray;
+//import net.sf.json.JSONObject;
+
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class RPCConnection {
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+    private final String conHost="10.4.21.103";
+    private final String conUsername="wucheng";
+    private final String conPassword="123456";
+
+    public RPCConnection() throws Exception {
+
+        // 初始化连接
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost(conHost);
+        factory.setUsername(conUsername);
+        factory.setPassword(conPassword);
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    /**
+     * 发送消息至requestQueueName队列中，并监听replyQueueName队列，取出replyQueueName队列中的消息
+     * @param message 客户端向服务器端请求的消息
+     * @return 从replyQueueName队列中得到的消息
+     * @throws Exception
+     */
+    public String call(String message) throws Exception {
+        String response = null;
+        String corrId = UUID.randomUUID().toString();
+
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        //向服务器端队列发送消息
+        channel.basicPublish("", requestQueueName, props, message.getBytes());
+
+        //客户端对RPC的超时处理
+        while (true) {
+            // 阻塞监听replyQueueName队列,3s之后无响应即超时
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery(3000);
+
+            if(delivery==null){  //3秒之后无返回
+                response="{\"msg\":\" Error: 服务器连接失败\"}";
+                break;
+            }
+            else if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(),"UTF-8");
+                break;
+            }
+        }
+        return response;
+    }
+
+    /**
+     * 关闭channel和connection连接
+     * @throws Exception
+     */
+    public void close() throws Exception {
+        channel.close();
+        connection.close();
+    }
+
+    /**
+     * 将获得的json数据进行解析，并显示在界面上
+     * @param     json  json字符串
+     */
+    /*
+    public void jsonparser(String json){
+        String code,msg,time,size;
+        JSONObject jsonObj;
+        try {
+            // 获取整个json字符串对象
+            jsonObj = JSONObject.fromObject(json);
+
+            code = jsonObj.getString("code");
+            msg = jsonObj.getString("msg");
+            time = jsonObj.getString("time");
+            size = jsonObj.getString("size");
+        }catch (Exception e)
+        {
+            System.out.println("Error: 从服务端获取数据出错");
+            return;
+        }
+
+        if(Integer.valueOf(code) == 0) {
+            // 获取字符串对象中的result数组
+            JSONArray jsonArray = jsonObj.getJSONArray("result");
+            if (null != jsonArray && jsonArray.size() > 0) {
+                List<Map<String, Object>> mapListJson = (List) jsonArray;
+
+                // 得到表头
+                ArrayList<String> headerList = new ArrayList<String>();
+                Map<String, Object> obj1 = mapListJson.get(0);
+
+                // TODO: 需要优化
+                for (Map.Entry<String, Object> entry : obj1.entrySet()) {
+                    String strkey1 = entry.getKey();    // 表头
+                    headerList.add(strkey1);
+                }
+                // 表头数组
+                String[] header = new String[headerList.size()];
+                header = headerList.toArray(header);
+
+
+                // 得到表中数据内容存在二维数组中
+                String[][] data = new String[mapListJson.size()][headerList.size()];
+                int j;
+                for (int i = 0; i < mapListJson.size(); i++) {
+                    Map<String, Object> obj = mapListJson.get(i);
+                    j = 0;
+                    for (Map.Entry<String, Object> entry : obj.entrySet()) {
+                        Object strval1 = entry.getValue();
+                        data[i][j++] = strval1.toString();
+                    }
+                }
+
+                // 生成TextTable
+                TextTable tt = new TextTable(header, data);
+
+                // Add the numbering on the left
+                tt.setAddRowNumbering(true);
+
+                // Print table
+                tt.printTable();
+                System.out.println();
+                System.out.println("SQL Execution Time: " + time + "  HDFS R&W Size: " + size);
+            }
+        }
+        else{
+            if(msg.contains("SocketException")) {
+                System.out.println("Error: 与服务器连接失效，请重新连接");
+            }else {
+                System.out.println("Error: " + msg);
+            }
+        }
+    }*/
+}
