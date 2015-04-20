32bfb1a177bfc204edad0334bb22881d2685aa34
ihainan <ihainan72@gmail.com>
2015-04-20 23:01:24 +0800

    Mod: 注释规范化

--- src/main/java/service/JerseyServer.java
+++ src/main/java/service/JerseyServer.java
@@ -1,35 +1,38 @@
 package service;
 
 /**
+ * RESTful 服务器资源路由表
  * Created by orange on 2015/3/17.
  */
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("/service")
 public class JerseyServer {
 
-    RPCConnection rpcCon=null;
+    RPCConnection rpcCon = null;
 
     /**
      * 接受传入的SQL语句，然后执行SQL语句，return结果
-     * @param     sqlstring  传入的SQL语句
-     * @return    返回SQL语句查询的结果
+     *
+     * @param sqlString 传入的SQL语句
+     * @return 返回SQL语句查询的结果
      */
     @GET
     @Path("/sqlExecute")
-    public Response sqlExecute(@QueryParam("sql") String sqlstring) {
-        String response=null;
+    public Response sqlExecute(@QueryParam("sql") String sqlString) {
+        String response;
         try {
             if (rpcCon == null) {
                 rpcCon = new RPCConnection();
             }
-            sqlstring="{\"service\":\"sqlExecute\",\"sql\":\""+sqlstring+"\"}";
-            response = rpcCon.call(sqlstring);
+            sqlString = "{\"service\":\"sqlExecute\",\"sql\":\"" + sqlString + "\"}";
+            response = rpcCon.call(sqlString);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            response="{\"code\":\"1\",\"msg\":\"服务器内部出错\"}";
+            response = "{\"code\":\"1\",\"msg\":\"服务器内部出错\"}";
         }
         return Response.status(200).entity(response).build();
     }
@@ -37,9 +40,10 @@ public class JerseyServer {
 
     /**
      * 校验用户名与密码，并返回结果
-     * @param     password  即用户名
-     * @param     name  即密码
-     * @return    用户名与密码的验证结果
+     *
+     * @param password 即用户名
+     * @param name     即密码
+     * @return 用户名与密码的验证结果
      */
     @GET
     @Path("/check")
@@ -47,21 +51,23 @@ public class JerseyServer {
     public Response check(@QueryParam("name") String name,
                           @QueryParam("password") String password) {
         //获取name和password
-        String checkString="{\"name\":\""+name+"\", \"password\":\""+password+"\", \"service\":\"check\"}";//密码校验
-        String response = null;
+        String checkString = "{\"name\":\"" + name + "\", \"password\":\"" + password + "\", \"service\":\"check\"}";//密码校验
+        String response;
         try {
             if (rpcCon == null) {
                 rpcCon = new RPCConnection();
             }
 
             System.out.println(checkString);
+
             // 调用插入队列的函数
             response = rpcCon.call(checkString);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            response="{\"code\":\"1\",\"msg\":\"服务器内部出错\"}";
+            response = "{\"code\":\"1\",\"msg\":\"服务器内部出错\"}";
         }
-        // result属于json格式
+
+        // result属于 JSON 格式
         return Response.status(200).entity(response).build();
     }
 }
--- src/main/java/service/RPCConnection.java
+++ src/main/java/service/RPCConnection.java
@@ -1,35 +1,28 @@
-package service; /**
- * Created by orange on 2015/3/27.
- */
+package service;
 
 import com.rabbitmq.client.*;
-//import dnl.utils.text.table.TextTable;
-//import net.sf.json.JSONArray;
-//import net.sf.json.JSONObject;
 
-//import org.json.JSONArray;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
 import java.util.UUID;
 
+/**
+ * 本类用于建立到 RPCServer 端的连接
+ */
 public class RPCConnection {
 
     private Connection connection;
     private Channel channel;
-    private String requestQueueName = "rpc_queue";
     private String replyQueueName;
     private QueueingConsumer consumer;
-    private final String conHost="localhost";
-    private final String conUsername="guest";
-    private final String conPassword="guest";
+
+    // TODO: 从配置文件中读取
+    private String requestQueueName = "rpc_queue";
+    private final String conHost = "localhost";
+    private final String conUsername = "guest";
+    private final String conPassword = "guest";
 
     public RPCConnection() throws Exception {
-
-        // 初始化连接
+        // 初始化到 RPC 队列服务器的连接
         ConnectionFactory factory = new ConnectionFactory();
 
         factory.setHost(conHost);
@@ -45,12 +38,14 @@ public class RPCConnection {
 
     /**
      * 发送消息至requestQueueName队列中，并监听replyQueueName队列，取出replyQueueName队列中的消息
+     *
      * @param message 客户端向服务器端请求的消息
      * @return 从replyQueueName队列中得到的消息
-     * @throws Exception
+     * @throws IOException          发送消息失败
+     * @throws InterruptedException
      */
-    public String call(String message) throws Exception {
-        String response = null;
+    public String call(String message) throws IOException, InterruptedException {
+        String response;
         String corrId = UUID.randomUUID().toString();
 
         AMQP.BasicProperties props = new AMQP.BasicProperties
@@ -59,20 +54,19 @@ public class RPCConnection {
                 .replyTo(replyQueueName)
                 .build();
 
-        //向服务器端队列发送消息
+        // 向服务器端队列发送消息
         channel.basicPublish("", requestQueueName, props, message.getBytes());
 
-        //客户端对RPC的超时处理
+        // 客户端对RPC的超时处理
         while (true) {
             // 阻塞监听replyQueueName队列,3s之后无响应即超时
             QueueingConsumer.Delivery delivery = consumer.nextDelivery(3000);
 
-            if(delivery==null){  //3秒之后无返回
-                response="{\"code\":\"1\",\"msg\":\" Error: RPC服务器出现异常\"}";
+            if (delivery == null) {  //3秒之后无返回
+                response = "{\"code\":\"1\",\"msg\":\" Error: RPC服务器出现异常\"}";
                 break;
-            }
-            else if (delivery.getProperties().getCorrelationId().equals(corrId)) {
-                response = new String(delivery.getBody(),"UTF-8");
+            } else if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(), "UTF-8");
                 break;
             }
         }
@@ -81,7 +75,8 @@ public class RPCConnection {
 
     /**
      * 关闭channel和connection连接
-     * @throws Exception
+     *
+     * @throws Exception 关闭连接失败
      */
     public void close() throws Exception {
         channel.close();
